504aca655c58640e45015ef9990ed0b7706fea76
Your Name <shaxuezheng@foxmail.com>
2018-02-07 00:55:26 +0800

    配合移动麒麟板做了修改

--- Unicorn-V1.0/settings/Test FreeModbus.Debug.cspy.bat
+++ Unicorn-V1.0/settings/Test FreeModbus.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"D:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "E:\Code-version-Library\foodsafety\FreeModbus+FreeRTOS-8.0\settings\Test FreeModbus.Debug.general.xcl" --backend -f "E:\Code-version-Library\foodsafety\FreeModbus+FreeRTOS-8.0\settings\Test FreeModbus.Debug.driver.xcl" 
+"D:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "E:\Code-version-Library\foodsafety\Unicorn-V1.0\settings\Test FreeModbus.Debug.general.xcl" --backend -f "E:\Code-version-Library\foodsafety\Unicorn-V1.0\settings\Test FreeModbus.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"D:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "E:\Code-version-Library\foodsafety\FreeModbus+FreeRTOS-8.0\settings\Test FreeModbus.Debug.general.xcl" "--debug_file=%~1" --backend -f "E:\Code-version-Library\foodsafety\FreeModbus+FreeRTOS-8.0\settings\Test FreeModbus.Debug.driver.xcl" 
+"D:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "E:\Code-version-Library\foodsafety\Unicorn-V1.0\settings\Test FreeModbus.Debug.general.xcl" "--debug_file=%~1" --backend -f "E:\Code-version-Library\foodsafety\Unicorn-V1.0\settings\Test FreeModbus.Debug.driver.xcl" 
 
 @echo off 
 :end
